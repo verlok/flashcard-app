1f178c670b6728bd22d19dcdeb52a2dcc1721ea6
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2016-07-11 16:06:13 +0200

    Refactoring using combineReducers - with comments

--- public/bundle.js
+++ public/bundle.js
@@ -1,8 +1,31 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
-// Creating a store!
-var store = Redux.createStore(function (state, action) {
+/*
+{
+    // It's better to have top-level properties, as flat as you can have,
+    // instead of having a lot of nested objects
+    // Then you create a reducer for each state property
+    // and combine reducers using combineReducers
+
+    cards: [{
+        id: 123,
+        front: '',
+        back: '',
+        deckId: 123
+    }],
+    decks: [{
+        id: 123,
+        ...
+    }],
+
+    // these properties also exists in the router, but we copy them in the state
+    selectedDeckId: 123,
+    studyMode: true/false
+}
+*/
+
+var cards = function cards(state, action) {
     // this is a reducer - it takes an action and change the state
     switch (action.type) {
         case "ADD_CARD":
@@ -10,13 +33,25 @@ var store = Redux.createStore(function (state, action) {
                 score: 1,
                 id: +new Date()
             });
-            return Object.assign({}, state, {
-                cards: state.cards ? state.cards.concat([newCard]) : [newCard]
-            });
+            return state.concat([newCard]);
         default:
-            return state || {};
+            return state || [];
     }
-});
+};
+
+// Creating a store!
+var store = Redux.createStore(Redux.combineReducers({
+    cards: cards // equivalent of cards: cards
+    /*
+    // combineReducers is the equivalent of manually doing:
+    function (state, action) {
+        return {
+            cards: cards(state.cards, action),
+            docks: docks(state.docks, action)
+        }
+    }
+    */
+}));
 
 // Watch the store for changes
 store.subscribe(function () {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
-// Creating a store!
-const store = Redux.createStore(function (state, action ){
+/*
+{
+    // It's better to have top-level properties, as flat as you can have,
+    // instead of having a lot of nested objects
+    // Then you create a reducer for each state property
+    // and combine reducers using combineReducers
+
+    cards: [{
+        id: 123,
+        front: '',
+        back: '',
+        deckId: 123
+    }],
+    decks: [{
+        id: 123,
+        ...
+    }],
+
+    // these properties also exists in the router, but we copy them in the state
+    selectedDeckId: 123,
+    studyMode: true/false
+}
+*/
+
+const cards = (state, action) => {
     // this is a reducer - it takes an action and change the state
     switch (action.type) {
         case "ADD_CARD":
@@ -7,13 +30,25 @@ const store = Redux.createStore(function (state, action ){
                 score: 1,
                 id: +new Date
             });
-            return Object.assign({}, state, {
-                cards: state.cards ? state.cards.concat([newCard]) : [newCard]
-            });
+            return state.concat([newCard]);
         default:
-            return state || {};
+            return state || [];
     }
-});
+};
+
+// Creating a store!
+const store = Redux.createStore(Redux.combineReducers({
+    cards // equivalent of cards: cards
+    /*
+    // combineReducers is the equivalent of manually doing:
+    function (state, action) {
+        return {
+            cards: cards(state.cards, action),
+            docks: docks(state.docks, action)
+        }
+    }
+    */
+}));
 
 // Watch the store for changes
 store.subscribe(() => {
